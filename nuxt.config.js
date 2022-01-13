d0d6ad4800cde69a1fd51509b6346bcb901d5499
SirLewyn <lewyn@tuta.io>
2022-01-13 02:09:20 -0300

    Vercel fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,26 +1,28 @@
 export default {
-    head: {
-      title: 'Avenida Automóveis',
-      titleTemplate: 'Veículos seminovos em Indaiatuba - SP | %s',
-      htmlAttrs: {
-        lang: 'pt-br',
-        amp: true
-      },
-      meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        {
-          hid: 'avenida',
-          name: 'description',
-          content: 'Seminovos em Indaiatuba - São Paulo'
-        }
-      ],
-    },
+  target: 'static',
 
-    buildModules: [
-      '@nuxtjs/style-resources',
+  head: {
+    title: 'Avenida Automóveis',
+    titleTemplate: 'Veículos seminovos em Indaiatuba - SP | %s',
+    htmlAttrs: {
+      lang: 'pt-br',
+      amp: true
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'avenida',
+        name: 'description',
+        content: 'Seminovos em Indaiatuba - São Paulo'
+      }
     ],
-    styleResources: {
-      scss: ['./assets/scss/main.scss']
-    }
+  },
+
+  buildModules: [
+    '@nuxtjs/style-resources',
+  ],
+  styleResources: {
+    scss: ['./assets/scss/main.scss']
   }
+}
