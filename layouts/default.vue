3bb41eb2a38dac6209f6ddd679d3ad9ff31d6d70
VnySrc <vinyprog.work@gmail.com>
2022-08-29 10:55:25 -0300

    Update imgs/facebook pixel

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,22 +51,6 @@
     </div>
 </template>
     <div id="fb-root"></div>
-    <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '{n2p890nsirhpvwm80jfag4cjwwlpkq}');
-    fbq('track', 'PageView');
-  </script>
-  <noscript>
-    <img height="1" width="1" style="display:none" 
-         src="https://www.facebook.com/tr?id={n2p890nsirhpvwm80jfag4cjwwlpkq}&ev=PageView&noscript=1"/>
-  </noscript>
 <script>
 import { mapState } from "vuex";
 
