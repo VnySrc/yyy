c515c0c232589fbf5d287e5829c2591fc47be173
VnySrc <vinyprog.work@gmail.com>
2022-08-29 11:43:16 -0300

    Update imgs/facebook pixel

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,12 +44,6 @@ export default {
       }
     ]
   },
-  hooks: {
-    'render:route': (_url, result) => {
-      result.html = result.html
-        .replace(/ data-n-head=".*?"/gi, '')
-    }
-  },
   plugins: [
     { src: '~/plugins/vue-fb-customer-chat.js', ssr: false }
   ],
